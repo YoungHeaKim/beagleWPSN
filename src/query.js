2e4024e2512f0854780eb4fcb01588c204617e36
JeongwonKim <swtpumpkin90@gmail.com>
2017-10-11 16:58:53 +0900

    passport의 facebook과 관련하여 query.js, passport.js 파일 수정

--- src/query.js
+++ src/query.js
@@ -1,5 +1,35 @@
 const knex = require('./knex')
 
 module.exports = {
-  
+  firstOrCreateUserByProvider(provider, provider_user_id, access_token=null, profile_photo=null) {
+    return knex('user')
+      .where({
+        provider,
+        provider_user_id
+      })
+      .first()
+      .then(user => {
+        if (user) {
+          return user
+        } else {
+          return knex('user')
+            .insert({
+              provider,
+              provider_user_id,
+              access_token,
+              profile_photo
+            })
+            .then(([id]) => {
+              return knex('user')
+                .where({id})
+                .first()
+            })
+        }
+      })
+  },
+  getUserById(id) {
+    return knex('user')
+      .where({id})
+      .first()
+  },
 }
