2702a0208128b1c182fe4ffd6f493b8f2c0e070c
yujin-min <yujinmin91@gmail.com>
2017-10-18 11:31:11 +0900

    채팅방 정보를 받아와 앞단에 넘겨주기 위해 기본 세팅

--- src/chatquery.js
+++ src/chatquery.js
@@ -22,8 +22,22 @@ module.exports = {
       })
   },
   getRoomById(id) {
+    // 2번
     return knex('chat_room')  
       .where({id})
       .first()
+  },
+  IsCreator() {
+    // 3번
+    // getRoomById에 온 정보와 앞단에선 넘어온 유저 정보(아마도 params)를 비교 
+  },
+  getRoomInfoByJoin() {
+    // 4번 
+    // innerjoin 사용 
+  },
+  findOrCreateChatList() {
+    // 1번 
+    // 앞단에서 넘어온 유저 정보로 챗 리스트에 이미 정보가 존재하는지 아닌지 파악 
+    // 채팅방 룸 아이디로 챗 리스트 테이블의 유저들을 가져온 다음에 존재하는지 파악하면 된다. 
   }
 }
