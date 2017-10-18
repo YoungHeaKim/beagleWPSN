7eba2f610aa87898ab443ca5bc16f7e0df9c6e2f
YoungHeaKim <dydgo9507@gmail.com>
2017-10-18 15:37:58 +0900

    프로필 부분 대화방 삭제 부분 query를 작성하고 profile.js의 기본세팅 완료

--- src/profilequery.js
+++ src/profilequery.js
@@ -2,5 +2,16 @@
 cosnt knex = require('./knex.js')
 
 module.exports = {
+  // 채팅방 삭제하는 코드
+  deleteRoom(id) {
+    return knex('chat_room')
+      .where({id})
+      .delete()
+  },
+  // 해당 user에 user정보와 참가하고 있는 room정보를 가져온다.
+  getProfile() {
+    return knex('user')
+  }
+  // user정보 수정하는 코드
 
 }
--- src/router/profile.js
+++ src/router/profile.js
@@ -1,8 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const {getUserById} = require('../authquery')
+const {getRoomById} = require('../chatquery')
+const query = require('../profilequery')
 
 const router = express.Router()
+
+router.use(bodyParser.json())
+
+router.use(bodyParser.urlencoded({ extended: false }))
+
+// cors오류 문제 해결해주는 코드
+router.use(cors({
+  origin: process.env.TARGET_ORIGIN
+}))
+
+// 로그인이 되어있는 유저 정보를 가져와야한다.
+// 로그인이 되어있는 유저가 들어가있는 방 정보를 가져온다.
+router.get('/', (req, res, next) => {
+  // 자기 자신의 프로필을 가져온다.
+  // getUserById(req.params.id)
+  //   .then(user => {
+  //     if(user) {
+  //       res.json(user)
+  //     } else {
+  //       res.status(404).send('User로그인 정보가 다릅니다.')
+  //     }
+  //   }),
+  // getRoomById(req.params.id)
+  //   .then(room => {
+  //     if (room) {
+  //       res.json(room)
+  //     } else {
+  //       res.status(404).send('Room Not Found')
+  //     }
+    })
+})
 
 
 
