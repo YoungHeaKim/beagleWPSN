599e160d011e03ab577905f57be3e276351f44ef
yujin-min <yujinmin91@gmail.com>
2017-10-18 11:07:37 +0900

    knex에 add_chat_log, add_chat_list 추가. 데이터베이스 완성.

---
+++ migrations/20171018105054_add_chat_list.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('chat_list', t => {
+    t.integer('user_id').unsigned().notNullable()
+    t.foreign('user_id').references('user.id')
+    t.integer('chat_room_id').unsigned().notNullable()
+    t.foreign('chat_room_id').references('chat_room.id')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('chat_list')
+};
