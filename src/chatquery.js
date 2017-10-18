bf335fb4bd4f2e935939dda9eb4f27f0aec18818
yujin-min <yujinmin91@gmail.com>
2017-10-18 11:42:07 +0900

    createRoom 할 시에 크리에이터를 chat_list 테이블에 추가시키는 로직 추가

--- src/chatquery.js
+++ src/chatquery.js
@@ -16,8 +16,17 @@ module.exports = {
         city_id
       })
       .then(([id]) => {
+        return knex('chat_list')
+          .insert({
+            user_id: creator,
+            chat_room_id: id
+          })
+      })
+      .then(([chat_room_id]) => {
         return knex('chat_room')
-          .where({id})
+          .where({
+            id: chat_room_id
+          })
           .first()
       })
   },
@@ -29,7 +38,7 @@ module.exports = {
   },
   IsCreator() {
     // 3번
-    // getRoomById에 온 정보와 앞단에선 넘어온 유저 정보(아마도 params)를 비교 
+    // getRoomById에 온 정보와 앞단에선 넘어온 유저 정보(아마도 토큰 정보)를 비교 
   },
   getRoomInfoByJoin() {
     // 4번 
@@ -37,7 +46,7 @@ module.exports = {
   },
   findOrCreateChatList() {
     // 1번 
-    // 앞단에서 넘어온 유저 정보로 챗 리스트에 이미 정보가 존재하는지 아닌지 파악 
+    // 앞단에서 넘어온 유저 정보(토큰 정보 확인)로 챗 리스트에 이미 정보가 존재하는지 아닌지 파악 
     // 채팅방 룸 아이디로 챗 리스트 테이블의 유저들을 가져온 다음에 존재하는지 파악하면 된다. 
   }
 }
