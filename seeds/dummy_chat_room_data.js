f5ca35a74d40692be7894a5ff3df6ab6d0f395f9
JeongwonKim <swtpumpkin90@gmail.com>
2017-10-19 09:01:15 +0900

    faker를 사용하여 dummy data를 생성하였다. chat_room_data는 에러를 확인하지 못했고 data가 들어가지 않는다. 추후 확인 예정이다.

--- seeds/dummy_chat_room_data.js
+++ seeds/dummy_chat_room_data.js
@@ -1,13 +1,23 @@
+const faker = require('faker')
+faker.locale = 'ko'
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  return knex('chat_room').del()
+  then(() => {
+    const arr = []
+    for(var i = 1; i <= 10; i++) {
+      arr.push(
+        knex('chat_room')
+        .insert({
+          name: faker.lorem.word(),
+          description: faker.lorem.sentences(),
+          start_at: faker.date.between('2016-01-01', '2017-11-01'),
+          photo: faker.image.city(),
+          creator: Math.floor(Math.random()*1000)+1,
+          city_id: Math.floor(Math.random()*19)+1,
+        })
+      )
+    }
+    return Promise.all(arr)
+  })
+}
