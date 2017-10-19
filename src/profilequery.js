d549a64dc97e345b75fecf6060a34451cc766aba
YoungHeaKim <dydgo9507@gmail.com>
2017-10-19 22:25:33 +0900

    여러개의 대화방을 가져올 수 있는 대화방 쿼리를 작성했다.

--- src/profilequery.js
+++ src/profilequery.js
@@ -8,6 +8,12 @@ module.exports = {
       .where({id})
       .delete()
   },
+  // 여러개의 대화방을 가져올 수 있는 대화방 쿼리
+  getRoomsById(id) {
+    return knex('chat_room')
+      .where({creater})
+      .first()
+  }
   // user정보 수정하는 코드
 
 }
--- src/router/profile.js
+++ src/router/profile.js
@@ -23,7 +23,14 @@ router.get('/', (req, res, next) => {
   // 자기 자신의 프로필을 가져온다.
   const user = getUserById(req.user.id)
   // 여러개의 대화방을 가져올 수 있는 대화방 쿼리를 짜주어야한다.
+  const room = getRoomById()
+    .then(
+      if(creater) {
 
+      } else {
+
+      }
+    )
   Promise.all([user, room])
     .then()
 })
