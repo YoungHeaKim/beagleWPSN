fec0f64c70ff92ba54d506fba4e5acf01f6f823a
YoungHeaKim <dydgo9507@gmail.com>
2017-10-20 15:42:33 +0900

    creator를 한번에 여러사람이 db를 랜덤으로 생성하기 위해 코드 수정

--- seeds/dummy_chat_room_data.js
+++ seeds/dummy_chat_room_data.js
@@ -2,7 +2,7 @@ const faker = require('faker')
 faker.locale = 'ko'
 
 exports.seed = function(knex, Promise) {
-  return knex('chat_room').del()
+  return knex('chat_room')
   .then(() => {
     const arr = []
     // 한번 실행시 만개의 더미데이터 생성합니다.
@@ -15,9 +15,10 @@ exports.seed = function(knex, Promise) {
           start_at: faker.date.between('2016-01-01', '2017-11-01'),
           photo: faker.image.city(),
           // creator가 계속 변하므로 user.id확인 후 한 계정당 하나씩의 db생성바랍니다.
+          // 이때 우리는 한 계정당 하나씩 db생성하는 부분이 힘들기 때문에 한번에 여러개의 유저에 대한 db를 랜덤을 돌려 생성을 한다.
           // creator의 숫자는 user.id 숫자 이므로 수정하면서 seed:run 바랍니다.
           // initial_cities는 한번만 실행하시기 바랍니다.
-          creator: 1,
+          creator: Math.floor(Math.random()*10)+1,
           city_id: Math.floor(Math.random()*20)+1,
         })
       )
