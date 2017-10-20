98024bb9ea9ed613839659870257d8741640fbdf
JeongwonKim <swtpumpkin90@gmail.com>
2017-10-20 15:32:50 +0900

    프론트에서 요청하는 chat-list 쿼리 완성 선택된 요청에 대한 쿼리는 작성해야함.

--- src/mainquery.js
+++ src/mainquery.js
@@ -1,5 +1,10 @@
 const knex = require('./knex')
 
 module.exports = {
-
+  getAllRoomList () {
+    return knex('user')
+      .join('chat_room', 'chat_room.creator', 'user.id')
+      .join('city', 'chat_room.city_id', 'city.id')
+      .select('chat_room.id', 'chat_room.name', 'chat_room.description', 'chat_room.start_at', 'chat_room.photo', 'user.nickname', 'user.profile_photo', 'user.like', 'city.city_name', 'city.city_photo')
+   },
 }
