0fbb0b7a22f59465639453bf3d8ce9f13ed900d6
JeongwonKim <swtpumpkin90@gmail.com>
2017-10-20 18:55:35 +0900

    프론트에서 넘어오는 data의 분기를 설정 완료함.

--- src/mainquery.js
+++ src/mainquery.js
@@ -5,6 +5,38 @@ module.exports = {
     return knex('user')
       .join('chat_room', 'chat_room.creator', 'user.id')
       .join('city', 'chat_room.city_id', 'city.id')
-      .select('chat_room.id', 'chat_room.name', 'chat_room.description', 'chat_room.start_at', 'chat_room.photo', 'user.nickname', 'user.profile_photo', 'user.like', 'city.city_name', 'city.city_photo')
+      .select('chat_room.id', 'chat_room.city_id', 'chat_room.name', 'chat_room.description', 'chat_room.start_at', 'chat_room.photo', 'user.nickname', 'user.profile_photo', 'user.like', 'city.city_name', 'city.city_photo')
    },
+   getDataRoomList ({city_id, start_at, like, id}) {
+    let query = this.getAllRoomList()
+    if(city_id && start_at){
+      if(like){
+        query = query.where({city_id, start_at})
+        query = query.orderBy('like', 'desc')
+      }else if(id){
+        query = query.where({city_id, start_at})
+        query = query.orderBy('id', 'desc')
+      }
+    }else if (city_id) {
+      if(like){
+        query = query.where({city_id})
+        query = query.orderBy('like', 'desc')
+      }
+      else if(latest){
+        query = query.where({city_id})
+        query = query.orderBy('like')
+      }
+    }else if (start_at) {
+      if(like){
+        query = query.where({start_at})
+        query = query.orderBy({like})
+      }
+      else if(latest){
+        query = query.where({start_at})
+        query = query.orderBy({latest})
+      }
+    }
+    return query
+      .select('chat_room.id', 'chat_room.name', 'chat_room.description', 'chat_room.start_at', 'chat_room.photo', 'user.nickname', 'user.profile_photo', 'user.like', 'city.city_name', 'city.city_photo')
+  },
 }
