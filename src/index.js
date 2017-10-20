b9acd23ed795755407a5dd705ed22a568ff254d3
yujin-min <yujinmin91@gmail.com>
2017-10-20 18:10:46 +0900

    jwt 토큰 테스트를 위해 로직 작성.  소켓아이오 추가 완료

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 require('dotenv').config()
 
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+const socketioJwt = require('socketio-jwt')
+
 const authRouter = require('./router/auth')
 const loginRouter = require('./router/login')
 const roomsRouter = require('./router/room')
 const profileRouter = require('./router/profile')
 const mainRouter = require('./router/main')
+const chatConnect = require('./router/chat')
 
 const app = express()
+const server = http.Server(app)
+const io = socketio(server)
 
 const PORT = process.env.PORT || 3000
 
 app.set('trust proxy')
+
+// io.use(socketioJwt.authorize({
+//   secret: process.env.SECRET,
+//   handshake: true
+// }))
+
+// io.use((socket, next) => {
+//   if (socket.decoded_token.id) {
+//     next()
+//   } else {
+//     next(new Error('Authentication Error'))
+//   }
+// })
+
+chatConnect(io)
 
 app.use('/auth', authRouter)
 app.use('/login', loginRouter)
@@ -22,6 +44,6 @@ app.use('/main', mainRouter)
 // 서버 테스트
 app.use('/test', function(req, res) { return res.send({ alive: true }) })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Able to connect to ${PORT}`)
 })
