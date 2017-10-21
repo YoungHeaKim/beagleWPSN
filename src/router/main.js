fc8a53d197e7be60c937320384f4d0ada6836de3
JeongwonKim <swtpumpkin90@gmail.com>
2017-10-21 21:21:54 +0900

    router 셋팅

--- src/router/main.js
+++ src/router/main.js
@@ -1,7 +1,14 @@
 const query = require('../mainquery')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const router = express.Router()
 
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: false }))
+
+router.post('/', (req, res) => {})
+
+router.get('/', (req, res, next) => {})
 
 module.exports = router
