2fc034c9e49c20e85520e21ac98d437399c06dc1
JeongwonKim <swtpumpkin90@gmail.com>
2017-10-22 18:44:49 +0900

    쿼리파일 수정 및 메인 라우터 기능 추가

--- src/mainquery.js
+++ src/mainquery.js
@@ -5,7 +5,7 @@ module.exports = {
     return knex('user')
       .join('chat_room', 'chat_room.creator', 'user.id')
       .join('city', 'chat_room.city_id', 'city.id')
-      .select('chat_room.id', 'chat_room.city_id', 'chat_room.name', 'chat_room.description', 'chat_room.start_at', 'chat_room.photo', 'user.nickname', 'user.profile_photo', 'user.like', 'city.city_name', 'city.city_photo')
+      .select('chat_room.city_id', 'chat_room.id', 'chat_room.name', 'chat_room.description', 'chat_room.start_at', 'chat_room.photo', 'user.nickname', 'user.profile_photo', 'user.like', 'city.city_name', 'city.city_photo')
    },
    getDataRoomList ({city_id, start_at, like, id}) {
     let query = this.getAllRoomList()
@@ -24,16 +24,16 @@ module.exports = {
       }
       else if(latest){
         query = query.where({city_id})
-        query = query.orderBy('like')
+        query = query.orderBy('latest', 'desc')
       }
     }else if (start_at) {
       if(like){
         query = query.where({start_at})
-        query = query.orderBy({like})
+        query = query.orderBy('like', 'desc')
       }
       else if(latest){
         query = query.where({start_at})
-        query = query.orderBy({latest})
+        query = query.orderBy('latest', 'desc')
       }
     }
     return query
--- src/router/main.js
+++ src/router/main.js
@@ -7,8 +7,23 @@ const router = express.Router()
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: false }))
 
-router.post('/', (req, res) => {})
+// index page를 켰을때, 모든 RoomList를 전송한다.
+router.post('/', (req, res) => {
+  res.send(query.getAllRoomList())
+})
 
-router.get('/', (req, res, next) => {})
+// 필터링에 대한 요청이 들어오게 되면 필터링 된 결과 값을 보내준다.
+router.get('/', (req, res, next) => {
+  query.getDataRoomList({
+    city_id: req.query.city_id,
+    start_at: req.query.start_at,
+    like: req.query.like,
+    id: req.query.id
+  }).then(list => {
+    res.send(list)
+  }).catch(err => {
+    done(err)
+  })
+})
 
 module.exports = router
