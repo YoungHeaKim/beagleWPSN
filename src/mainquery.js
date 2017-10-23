2e50e908529b417a66094eb3caaba829e621aa6f
JeongwonKim <swtpumpkin90@gmail.com>
2017-10-23 18:03:16 +0900

    채팅방 생성 순으로 기본 값 표출되도록 수정

--- src/mainquery.js
+++ src/mainquery.js
@@ -5,6 +5,7 @@ module.exports = {
     return knex('user')
       .join('chat_room', 'chat_room.creator', 'user.id')
       .join('city', 'chat_room.city_id', 'city.id')
+      .orderBy('chat_room.id', 'desc')
       .select('chat_room.city_id', 'chat_room.id', 'chat_room.name', 'chat_room.description', 'chat_room.start_at', 'chat_room.photo', 'user.nickname', 'user.profile_photo', 'user.like', 'city.city_name', 'city.city_photo')
       .limit(12)
    },
